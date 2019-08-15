acfa32bf3be23c19ccd7f93fbf8aac4fd1b4069d
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-15 23:21:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do 
+  erb :user_input
+end
+post '/piglatinize' do 
+  pl = Piglatinizer.new 
+  @piglatin = pl.piglatinize(params[:user_phrase])
+  erb :results 
+end 
+
+  
 end
